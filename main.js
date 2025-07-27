97870d7332d85b4b49d90c8475d71238066ecd0e
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-07-27 00:53:53 -0400

    Added calculator button  functionality: can now enter numbers and have them displayed can also clear what ever was entered into the display

--- main.js
+++ main.js
@@ -1,22 +1,60 @@
+let allClear = true;
+let operating = false;
+let firstNumber = 0;
+let secondNumber = 0;
 function add(num1, num2){
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2){
-    return num1 - num2
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2){
-    return num1 *num2;
+    return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2){
-    return num1 / num2;
+    return Number(num1) / Number(num2);
 }
 
 function operate(operator, num1, num2){
     return operator(num1, num2);
 }
 
+function displayNumber(number){
+    if(allClear){
+        allClear = false;
+        clearBtn.textContent = "C";
+        display.textContent = "";
+    }
+    display.textContent += number;
+}
 
-console.log(operate(divide,5,2))
+function clear(){
+    if(!allClear){
+        display.textContent = "0";
+        allClear = true;
+        clearBtn.textContent = "AC"
+    }
+}
+
+
+
+const display = document.querySelector("#display")
+
+const numbers = document.querySelectorAll(".number")
+for (let i = 0; i < numbers.length; i++){
+    let number = numbers[i];
+    number.addEventListener("click", ()=> displayNumber(number.textContent))
+    number.addEventListener("mousedown", () => number.style.backgroundColor = "#AAAAAA")
+    number.addEventListener("mouseup", () => number.style.backgroundColor = "#505050")
+    
+}
+
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click",() => clear())
+clearBtn.addEventListener("mousedown", () => clearBtn.style.backgroundColor = "#F4F4F2")
+clearBtn.addEventListener("mouseup", () => clearBtn.style.backgroundColor = "#D4D4D2")
+
+
